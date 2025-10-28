2d75a891e8c64f17051c1d4ba19ec6fcf12bdb83
Joan <Jr2614171@gmail.com>
2025-10-27 23:30:16 -0400

    Add environment files, update .gitignore and dependencies to fix Angular build

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  socketUrl: 'http://localhost:3000',
+  appVersion: 'DEV',
+  appName: 'GEOapp',
+
+  // --API BACKEND
+  // -- api GraphHopper, servicio de enrutamiento de código abierto basado en OpenStreetMap.
+  api_GraphHopper: 'ye4CXka4OF5wzWA4m5Sw17GAs-fvmzAMZrnEOrhE8c8',
+  
+  // -- api OpenRouteService, servicio de enrutamiento de código abierto basado en OpenStreetMap.
+  api_ors: '5b3ce3597851110001cf6248694920327f1c4d81ad3c9c7231c5a802'
+};
